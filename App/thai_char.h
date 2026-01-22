5d17bb0c53a6159d933e87540ff1020ddb2a9b8e
superogira / E25WOP <superogira@hotmail.com>
2026-01-22 10:20:58 +0700

    Add files via upload

---
+++ App/thai_char.h
@@ -0,0 +1,5 @@
+// ประกาศชื่อตัวอักษรให้เรียกง่ายๆ (ตัวอย่างบางส่วน)
+#define TH_SARA_E   "\xE0" // เ
+#define TH_SOR_SUA  "\xCA" // ส
+#define TH_TOR_TAO  "\xB5" // ต
+#define TH_POR_PLA  "\xBB" // ป
